5bb7df21555d1c9dfd0603fd4f19cc21acb265a9
vikram kumar mishra <vikram.mishra@mindfiresolutions.com>
2012-04-09 12:17:34 +0530

    Shri ganeshay namah

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MFSocial::Application.config.secret_token = 'ee2535b34cb9abf125337b88e720ce0b8d4ba9577c9b37ade5dec531a8dcfbd59d4c85c4b2cf60a11e897736a8b200ad43ac7c7bb0f657d2608daa5fc8155515'
